984245b8a87ca8dafce7f5fa1aab627e30afbd13
albarotti <ghabryel.albarotti@gmail.com>
2024-02-17 22:31:10 -0300

    commit 1

---
+++ aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'Ghabryel' + ' ' + 'Albarotti'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_ghabryel = 3 * 'Ghabryel'
+print(a_dez_vezes)
+print(tres_vezes_ghabryel)
---
+++ aula13.py
@@ -0,0 +1,17 @@
+nome = 'Ghabryel Albarotti'
+altura = 1.84
+peso = 98
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f}m de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.3f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Ghabryel Albarotti tem 1.84 de altura,
+# pesa 98 quilos e seu IMC é
+# 29.320987654320987
