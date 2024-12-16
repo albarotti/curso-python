0ca7fae0fda05e886fa5508627e9389fff6217b8
Ghabryel Albarotti <ghabryel.albarotti@gmail.com>
2024-12-16 15:52:50 -0300

    comitando alteracoes

---
+++ aula12.py
@@ -0,0 +1,12 @@
+nome = 'Ghabryel Albarotti'
+altura = 1.84
+peso = 96
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Ghabryl Albarotti tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
---
+++ aula3.py
@@ -0,0 +1,19 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+srt -> string -> texto
+String são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples 
+print('Ghabryel Albarotti')
+
+# Aspas duplas
+print("Ghabryel Albarotti")
+
+# Escape
+print("Ghabryel \"Albarotti\"")
+
+# r
+print(r"Ghabryel \"Albarotti\"")
